4c54b0b378250b8abbc8ce3f8f768be050914531
hoxi777 <jtho1015@gmail.com>
2019-05-16 15:42:05 +0900

    update wrong text

---
+++ src/api/User/createAccount/createAccount.js
@@ -0,0 +1,55 @@
+import { prisma } from "../../../../generated/prisma-client";
+
+export default {
+    Mutation: {
+        createAccount: async (_, args) => {
+            const { user_name,
+                    userid,
+                    password,
+                    re_passwored,
+                    nickname,
+                    birthday,
+                    gender,
+                    email,
+                    find_email = "",
+                    mailing = "",
+                    phone,
+                    cellPhone,
+                    zipCode,
+                    address
+                    } = args;
+
+            const exist = await prisma.$exists.user({
+                OR: [
+                    {
+                        nickname
+                    },
+                    {
+                        email
+                    }
+                ]
+            });
+
+            if (exist) {
+                throw Error("This nickname / email is already taken");
+            }
+            const user = await prisma.createUser({
+                user_name,
+                userid,
+                password,
+                re_passwored,
+                nickname,
+                birthday,
+                gender,
+                email,
+                find_email,
+                mailing,
+                phone,
+                cellPhone,
+                zipCode,
+                address
+            });
+            return user;
+        }
+    }
+}
